353ad4f0a09e4d595c85385bf4c8ca6a76efdb34
JinSoo Joo Mac <argentdarae@wardgames.co.kr>
2026-03-11 00:01:28 +0900

    feat: 홈 화면 추가

--- weak71/app/src/main/java/com/example/weak7_1/HomeActivity.java
+++ weak71/app/src/main/java/com/example/weak7_1/HomeActivity.java
@@ -6,7 +6,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

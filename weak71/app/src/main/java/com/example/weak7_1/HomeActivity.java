24ff3d13252848f8ae3d4a58d1e732bcb17e790b
JinSoo Joo Mac <argentdarae@wardgames.co.kr>
2026-03-11 14:35:44 +0900

    chore: Login 액티비티 생성

--- weak71/app/src/main/java/com/example/weak7_1/HomeActivity.java
+++ weak71/app/src/main/java/com/example/weak7_1/HomeActivity.java
@@ -11,7 +11,7 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
 
         Button btnLinear = findViewById(R.id.btnLinearLayout);
         Button btnConstraint = findViewById(R.id.btnConstraintLayout);
